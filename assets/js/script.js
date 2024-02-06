255a6ccc1ee01cdaa8b39e5b19157179bc0fc6a4
Aaron Ho <bicecreamaar@gmail.com>
2024-02-06 01:59:06 -0800

    started populating the tiles with images

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,17 +50,79 @@ const errorCallback = (error) => {
 // gets current location of client and calls the weather API if client allows access
 navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
-// create an array for shirts, jackets, pants, and shoes
-var shirtsImages = [];
-var jacketsImages = [];
-var pantsImages = [];
-var shoesImages = [];
-var currentShirt=0;
-var currentJacket=0;
-var currentPants=0;
-var currentShoes=0;
+// create an array for shirts, Tops, pants, and shoes
+var hatImages = ['https://placehold.co/600x500'];
+var topImages = ['https://placehold.co/600x500'];
+var bottomImages = ['https://placehold.co/600x500'];
+var shoeImages = ['https://placehold.co/600x500'];
+var hatTitles = ['placeholder image'];
+var topTitles = ['placeholder image'];
+var bottomTitles = ['placeholder image'];
+var shoeTitles = ['placeholder image'];
+var hatCurrent=0;
+var topCurrent=0;
+var bottomCurrent=0;
+var shoeCurrent=0;
 
 var linkButton = document.querySelector('#linkBtn');
+
+function populateImages() {
+  var cardEl1;
+  var cardEl2;
+  var cardEl3;
+  // populate hats
+  cardEl1 = document.getElementById('hat1');
+  cardEl2 = document.getElementById('hat2');
+  cardEl3 = document.getElementById('hat3');
+  if(hatImages.length > 2) { // if the list is 3 items or larger
+    if(hatCurrent == 0) {
+      cardEl1.classList.remove('is-invisible');
+      cardEl1.children[0].textContent = hatTitles[hatTitles.length-1];
+      cardEl1.children[1].children[0].src = hatImages[hatImages.length-1];
+      cardEl3.classList.remove('is-invisible');
+      cardEl3.children[0].textContent = hatTitles[hatCurrent+1];
+      cardEl3.children[1].children[0].src = hatImages[hatCurrent+1];
+    } else if(hatCurrent == hatImages.length-1) { // if the current item is the end of the list
+      cardEl1.classList.remove('is-invisible');
+      cardEl1.children[0].textContent = hatTitles[hatCurrent-1];
+      cardEl1.children[1].children[0].src = hatImages[hatCurrent-1];
+      cardEl3.classList.remove('is-invisible');
+      cardEl3.children[0].textContent = hatTitles[0];
+      cardEl3.children[1].children[0].src = hatImages[0];
+    } else { // if there's no edge cases to handle
+      cardEl1.classList.remove('is-invisible');
+      cardEl1.children[0].textContent = hatTitles[hatCurrent-1];
+      cardEl1.children[1].children[0].src = hatImages[hatCurrent-1];
+      cardEl3.classList.remove('is-invisible');
+      cardEl3.children[0].textContent = hatTitles[hatCurrent+1];
+      cardEl3.children[1].children[0].src = hatImages[hatCurrent+1];
+    }
+  } else if(hatImages.length == 2) { // if the list is 2 items
+    if(hatCurrent == 1) {
+      cardEl3.classList.add('is-invisible');
+      cardEl1.classList.remove('is-invisible');
+      cardEl1.children[0].textContent = hatTitles[hatCurrent-1];
+      cardEl1.children[1].children[0].src = hatImages[hatCurrent-1];
+    } else {
+      cardEl1.classList.add('is-invisible');
+      cardEl3.classList.remove('is-invisible');
+      cardEl3.children[0].textContent = hatTitles[hatCurrent+1];
+      cardEl3.children[1].children[0].src = hatImages[hatCurrent+1];
+    }
+  } else { // if the list only has the starting item
+    cardEl1.classList.add('is-invisible');
+    cardEl3.classList.add('is-invisible');
+  }
+  cardEl2.children[0].textContent = hatTitles[hatCurrent];
+  cardEl2.children[1].children[0].src = hatImages[hatCurrent];
+    
+  // populate tops
+
+  // populate bottoms
+
+  // populate shoes
+
+}
 
 function addImage() {
   var imageUrl = document.querySelector('#url-input').value;
@@ -68,24 +130,29 @@ function addImage() {
     return;
   }
   // pushes the url into the correct array and clears the input
-  if(document.getElementById('jackets').checked) {
-    jacketsImages.push(imageUrl.trim());
+  if(document.getElementById('hats').checked) {
+    hatImages.push(imageUrl.trim());
     document.getElementById('url-input').value = '';
-    console.log('jackets', jacketsImages);
-  } else if(document.getElementById('shirts').checked) {
-    shirtsImages.push(imageUrl.trim());
+    hatCurrent++;
+    console.log('hats', hatImages);
+  } else if(document.getElementById('tops').checked) {
+    topImages.push(imageUrl.trim());
     document.getElementById('url-input').value = '';
-    console.log('shirts', shirtsImages);
-  } else if(document.getElementById('pants').checked) {
-    pantsImages.push(imageUrl.trim());
+    topCurrent++;
+    console.log('tops', topImages);
+  } else if(document.getElementById('bottoms').checked) {
+    bottomImages.push(imageUrl.trim());
     document.getElementById('url-input').value = '';
-    console.log('pants', pantsImages);
+    bottomCurrent++;
+    console.log('bottoms', bottomImages);
   } else if(document.getElementById('shoes').checked) {
-    shoesImages.push(imageUrl.trim());
+    shoeImages.push(imageUrl.trim());
     document.getElementById('url-input').value = '';
-    console.log('shoes', shoesImages);
+    shoeCurrent++;
+    console.log('shoes', shoeImages);
   }
-
+  populateImages();
 }
 
+populateImages();
 linkButton.addEventListener('click', addImage);
